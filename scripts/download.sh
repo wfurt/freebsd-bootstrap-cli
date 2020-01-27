66534c19cc5617cee5cbdcf4686bfa6915624974
Adeel <adeelbm@outlook.com>
2020-01-27 14:31:02 +0200

    Apply miscellaneous fixes

--- scripts/download.sh
+++ scripts/download.sh
@@ -8,16 +8,15 @@ if [ ! -f ${ARTIFACTS_DIR}/download/dotnet-install.sh ]; then
 curl -o ${ARTIFACTS_DIR}/download/dotnet-install.sh  https://dot.net/v1/dotnet-install.sh
 fi
 
-#if [ ! -f ${ARTIFACTS_DIR}/download/xenial-server-cloudimg-amd64-vmlinuz-generic ]; then
-#  curl -L -o ${ARTIFACTS_DIR}/download/xenial-server-cloudimg-amd64-vmlinuz-generic https://cloud-images.ubuntu.com/xenial/current/unpacked/xenial-server-cloudimg-amd64-vmlinuz-generic
-#fi
+if [ ! -f ${ARTIFACTS_DIR}/download/xenial-server-cloudimg-amd64-vmlinuz-generic ]; then
+  curl -L -o ${ARTIFACTS_DIR}/download/xenial-server-cloudimg-amd64-vmlinuz-generic https://cloud-images.ubuntu.com/xenial/current/unpacked/xenial-server-cloudimg-amd64-vmlinuz-generic
+fi
 
-#if [ ! -f ${ARTIFACTS_DIR}/download/xenial-server-cloudimg-amd64-initrd-generic ]; then
-#  curl -L -o ${ARTIFACTS_DIR}/download/xenial-server-cloudimg-amd64-initrd-generic  https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-initrd-generic
-#fi
+if [ ! -f ${ARTIFACTS_DIR}/download/xenial-server-cloudimg-amd64-initrd-generic ]; then
+  curl -L -o ${ARTIFACTS_DIR}/download/xenial-server-cloudimg-amd64-initrd-generic  https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-initrd-generic
+fi
 
 mkdir -p ${ARTIFACTS_DIR}/linux
 if [ ! -f ${ARTIFACTS_DIR}/download/xenial-server-cloudimg-amd64-disk1.img ]; then
   curl -L -o ${ARTIFACTS_DIR}/download/xenial-server-cloudimg-amd64-disk1.img https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img
 fi
-
--- scripts/updateQemu.sh
+++ scripts/updateQemu.sh
@@ -18,5 +18,3 @@ runCmd apt update -y
 runCmd apt upgrade -y
 # build prerequisites
 runCmd apt install -y libunwind8 libicu55 curl git clang-3.9 cmake make libc6-dev libssl-dev libkrb5-dev libcurl4-openssl-dev zlib1g-dev liblldb-3.9-dev gettext libicu-dev liblttng-ust-dev libkrb5-dev
-
-
