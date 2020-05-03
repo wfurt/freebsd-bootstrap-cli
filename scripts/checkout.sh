bcaade2bf996cfa69b487a14cfc6448955911216
wfurt <tweinfurt@yahoo.com>
2020-05-02 19:12:45 -0700

    runtime patch

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -4,8 +4,11 @@ set -e
 
 . config
 
-if [ -z "$1" ] || [ "$1" == 'sdk' ] || [ "$1" == 'installer' ]; then
+if [ -z "$1" ] || [ "$1" == 'installer' ]; then
   git clone https://github.com/dotnet/installer --branch ${SDK_BRANCH} --single-branch --depth 1
+fi
+if [ -z "$1" ] || [ "$1" == 'sdk' ] ; then
+  git clone https://github.com/dotnet/sdk --branch ${SDK_BRANCH} --single-branch --depth 1
 fi
 if [ -z "$1" ] || [ "$1" == 'runtime' ]; then
   git clone https://github.com/dotnet/runtime --branch ${RUNTIME_BRANCH} --single-branch --depth 1
